d6ae0d6e5dbcf4e2726144fb378590238d6fb707
apollo_jhn <japollo.ponteras@gmail.com>
2022-10-24 00:20:23 +0800

    Second Commmit
    Bug's remove and google drive link has been removed, now the source is on local

--- Compile-and-Run.bat
+++
@@ -1,6 +0,0 @@
-:: Script was made by apollo-jhn(Ponteras, John Apollo S.)
-
-cd ../bin
-javac ../src/PLACEHOLDER.java
-appletviewer ../src/PLACEHOLDER.html
-pause
--- java.txt
+++
@@ -1,3 +0,0 @@
-import java.awt.*;
-import java.applet.*;
-import java.awt.event.*;
